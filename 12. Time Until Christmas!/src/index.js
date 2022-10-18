ce4d818467fa994df0537a3e36bc89b42e16848a
ukss <86929961+ukssss@users.noreply.github.com>
2022-10-19 01:15:37 +0900

    #20 Time Until Christmas (2)
    Vanilla JS Challenge Day 9

--- 12. Time Until Christmas!/src/index.js
+++ 12. Time Until Christmas!/src/index.js
@@ -2,30 +2,25 @@ const timer = document.querySelector("h2");
 
 function getClock() {
   const date = new Date();
-  const dday = new Date(2022, 12, 25);
+  const dday = new Date(`2022-12-25T00:00:00+0900`);
 
-  const ddayCal = dday.getTime() - date.getTime();
-  const days = ddayCal / (1000 * 60 * 60 * 24);
-  const hours = ddayCal / (1000 * 60 * 60);
-  const minutes = ddayCal / (1000 * 60);
-  const seconds = ddayCal / 1000;
+  const diffCal = dday.getTime() - date.getTime();
 
-  timer.innerText = `${days}d:${hours}h:${minutes}m:${seconds}s`; // 시:분:초
+  const diffSec = String(Math.floor((diffCal / 1000) % 60)).padStart(2, "0");
+  const diffMin = String(Math.floor((diffCal / (1000 * 60)) % 60)).padStart(2, "0");
+  const diffHour = String(Math.floor((diffCal / (1000 * 60 * 60)) % 24)).padStart(2, "0");
+  const diffDay = Math.floor(diffCal / (1000 * 60 * 60 * 24));
+
+  const isSameDay = (date, dday) => {
+    return date.getFullYear() === dday.getFullYear() && date.getMonth() === dday.getMonth() && date.getDate() === dday.getDate();
+  };
+
+  if (isSameDay(date, dday)) {
+    timer.innerText = "Merry Christmas!";
+  } else {
+    timer.innerText = `${diffDay}d : ${diffHour}h : ${diffMin}m : ${diffSec}s `; // 시:분:초
+  }
 }
 
 getClock(); // 최초 시간
 setInterval(getClock, 1000); // 1초 간격으로 시:분:초 출력
-
-// var now = new Date();
-
-// var year = now.getFullYear(); // 연도
-// var month = now.getMonth() + 1; // 월
-// var day = now.getDate(); // 일
-
-// var stDate = new Date(2021, 03, 21);
-// var endDate = new Date(year, month, day);
-
-// var btMs = endDate.getTime() - stDate.getTime();
-// var btDay = btMs / (1000 * 60 * 60 * 24);
-
-// console.log("일수 차이는?? " + btDay);
