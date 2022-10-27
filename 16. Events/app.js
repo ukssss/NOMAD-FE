962fc89ca54a0efadb311bf956ec32a2a9e259f8
ukss <86929961+ukssss@users.noreply.github.com>
2022-10-27 15:26:51 +0900

    #26 Events (2)
    채우기, 그리기 기능 선택 버튼 추가

--- 16. Events/app.js
+++ 16. Events/app.js
@@ -3,11 +3,13 @@ const ctx = canvas.getContext("2d");
 const lineWidth = document.getElementById("linewidth-range");
 const color = document.getElementById("color-change");
 const colorOptions = Array.from(document.querySelectorAll(".color-option"));
+const mode = document.getElementById("mode-btn");
 
 canvas.width = 800;
 canvas.height = 800;
 
 let setPaint = false;
+let setFill = false;
 ctx.lineWidth = lineWidth.value;
 
 function onMouseMove(event) {
@@ -46,6 +48,22 @@ function onChangeColorOption(event) {
   onColor(event.target.dataset.color);
 }
 
+function onChangeMode() {
+  if (setFill === false) {
+    setFill = true;
+    mode.innerText = "Fill";
+  } else {
+    setFill = false;
+    mode.innerText = "Draw";
+  }
+}
+
+function onCanvasFill() {
+  if (setFill === true) {
+    ctx.fillRect(0, 0, 800, 800);
+  }
+}
+
 canvas.addEventListener("mousemove", onMouseMove); // 마우스가 움직일 경우
 canvas.addEventListener("mousedown", onStartPaint); // 마우스가 클릭되었을 경우
 canvas.addEventListener("mouseup", onStopPaint); // 마우스 클릭이 해제되었을 경우
@@ -55,3 +73,6 @@ lineWidth.addEventListener("change", onChangeLineWidth); // input 값에 따라 
 color.addEventListener("change", onChangeColor); // input 값에 따라 색 변화
 
 colorOptions.forEach((color) => color.addEventListener("click", onChangeColorOption));
+
+canvas.addEventListener("click", onCanvasFill);
+mode.addEventListener("click", onChangeMode);
